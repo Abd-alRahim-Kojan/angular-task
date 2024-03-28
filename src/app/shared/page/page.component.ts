2ec5040345742caff326c4dab950e8a9ae1ffdad
Epixl Studio <abdalrahimk083@gmail.com>
2024-03-28 07:41:47 +0300

    edit page

--- src/app/shared/page/page.component.ts
+++ src/app/shared/page/page.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrl: './page.component.scss'
 })
-export class PageComponent {
-
-}
+export class PageComponent {}
